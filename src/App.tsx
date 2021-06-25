bc45b27c69fcdc2a6149de2305c931bb202ac210
Joaquim Amorim <joaquimkurama@gmail.com>
2021-06-25 20:48:02 -0300

    Letmeask

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
-
-import { createContext } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import { Home } from './pages/Home'
 import { NewRoom } from './pages/NewRoom'
+import { Room } from './pages/Room'
+import { AdminRoom } from './pages/AdminRoom'
 
-export const TestContext = createContext('');
+import { AuthContextProvider } from './contexts/AuthContext'
+
+
 
 function App() {
+
   return (
-    <BrowserRouter>
-        <TestContext.Provider value={'Teste'}>
+    <BrowserRouter>      
+      <AuthContextProvider>
+        <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/rooms/new" component={NewRoom} />
-        </TestContext.Provider>
+          <Route path="/rooms/:id" component={Room} />
+
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
+        </Switch>
+      </AuthContextProvider>  
     </BrowserRouter>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,46 @@
-import { useContext } from 'react';
 import { useHistory } from 'react-router-dom'
-
-import { auth, firebase } from '../services/firebase'
+import { useAuth } from '../hooks/UseAuth'
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 import {Button} from '../components/Button'
 
-import { TestContext } from '../App'
-
 import '../styles/auth.scss'
+import { FormEvent } from 'react'
+import { useState } from 'react'
+import { database } from '../services/firebase'
 
 export function Home(){
 
     const history = useHistory();
-    const value = useContext(TestContext)
+    const {user, signInWithGoogle} = useAuth()
+    const [roomCode, setRoomCode] = useState('');
 
-    function handleCreateRoom(){
-        const provider = new firebase.auth.GoogleAuthProvider();
+    async function handleCreateRoom(){
+        if(!user) {
+            await signInWithGoogle()
+        }
 
-        auth.signInWithPopup(provider).then(result => {
-            console.log(result);
 
-            history.push('/rooms/new')
-        })        
+        history.push('/rooms/new');
+    }
+
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if (roomCode.trim() === '') {
+            return;
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+        if (!roomRef.exists()) {
+            alert('Room does not exists.');
+            return;
+        }
+
+        history.push(`/rooms/${roomCode}`);
     }
 
     return (
@@ -43,10 +59,12 @@ export function Home(){
                         Crie sua sala com o Google
                     </button>
                     <div className="separator">Ou entre na sala</div>
-                    <form>
+                    <form onSubmit={handleJoinRoom}>
                         <input 
                             type="text" 
                             placeholder="Digite o código da sala"
+                            onChange={event => setRoomCode(event.target.value)}
+                            value={roomCode}
                         />
                         <Button type="submit">Entrar na sala</Button>
                     </form>
